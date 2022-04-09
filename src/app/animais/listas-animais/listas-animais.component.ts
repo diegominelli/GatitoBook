9b7a2bdae826a387f1ff1f29b064af3a316bba9a
Diego Minelli <diego.minelli@hotmail.com>
2022-04-09 19:13:56 -0300

    criando a lista dos animais

--- src/app/animais/listas-animais/listas-animais.component.ts
+++ src/app/animais/listas-animais/listas-animais.component.ts
@@ -1,15 +1,27 @@
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listas-animais',
   templateUrl: './listas-animais.component.html',
-  styleUrls: ['./listas-animais.component.css']
+  styleUrls: ['./listas-animais.component.css'],
 })
 export class ListasAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
-
 }
