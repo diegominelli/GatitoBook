7e0d5c1c6d921d2b0fb5fdcdd106bc6fc4dcbe41
Diego Minelli <diego.minelli@hotmail.com>
2022-04-09 20:39:29 -0300

    criando o componente de detalhe

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListasAnimaisComponent } from './listas-animais/listas-animais.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ListasAnimaisComponent,
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
   },
 ];
 
--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -6,9 +6,10 @@ import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListasAnimaisComponent } from './listas-animais/listas-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 @NgModule({
-  declarations: [ListasAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  declarations: [ListasAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
   imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
 export class AnimaisModule {}
