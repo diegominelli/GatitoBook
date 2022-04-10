5e93d7eff7af973cda8c0fa0bdd0ff5e35fc37f5
Diego Minelli <diego.minelli@hotmail.com>
2022-04-09 21:31:03 -0300

    usando o servico ActivatedRoute

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,15 +1,25 @@
+import { AnimaisService } from './../animais.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from '../animais';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
   templateUrl: './detalhe-animal.component.html',
-  styleUrls: ['./detalhe-animal.component.css']
+  styleUrls: ['./detalhe-animal.component.css'],
 })
 export class DetalheAnimalComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params['animalId'];
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
   }
-
 }
