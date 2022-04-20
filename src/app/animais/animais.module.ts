4cb5bb4025f2be7634d9157b5fcfd7d3ba0eee15
Diego Minelli <diego.minelli@hotmail.com>
2022-04-20 12:24:52 -0300

    criacao do servico comentarios

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -7,9 +7,16 @@ import { ListasAnimaisComponent } from './listas-animais/listas-animais.componen
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
 
 @NgModule({
-  declarations: [ListasAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
+  declarations: [
+    ListasAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent,
+  ],
   imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
 export class AnimaisModule {}
