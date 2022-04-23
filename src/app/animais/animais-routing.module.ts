4c1dee02a643b3623333b614ace1ebd837a8026c
Diego Minelli <diego.minelli@hotmail.com>
2022-04-23 09:43:54 -0300

    usando o guarda resolver

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListasAnimaisComponent } from './listas-animais/listas-animais.component';
 import { NgModule } from '@angular/core';
@@ -7,6 +8,9 @@ const routes: Routes = [
   {
     path: '',
     component: ListasAnimaisComponent,
+    resolve: {
+      animais: ListaAnimaisResolver,
+    },
   },
   {
     path: ':animalId',
